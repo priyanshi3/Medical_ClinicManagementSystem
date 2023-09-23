8d84b8b949d081d35bf3dfdafe374e515f228c7f
Priyanshi <parsaniyapriyanshi@gmail.com>
2023-09-23 15:55:51 +0530

    commit

---
+++ pages/receptionist/appointment.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="appointment.aspx.cs" Inherits="Medical_ClinicManagementSystem.pages.receptionist.Appointment" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            height: 26px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table class="auto-style1">
+            <tr>
+                <td>
+                    <asp:Label ID="lblFname" runat="server" Text="First Name"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="txtFname" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="lblLname" runat="server" Text="Last Name"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="txtLname" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    <asp:Label ID="lblSymptoms" runat="server" Text="Symptoms"></asp:Label>
+                </td>
+                <td class="auto-style2">
+                    <asp:TextBox ID="txtSymptoms" runat="server" TextMode="MultiLine"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="Submit" />
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ pages/receptionist/appointment.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Medical_ClinicManagementSystem.pages.receptionist
+{
+    public partial class Appointment : System.Web.UI.Page
+    {
+        String fname, lname, blood_group;
+        int age;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["Clinic"].ConnectionString;
+            try
+            {
+                using(con)
+                {
+                    string query = "select * from patient where first_name = '" + txtFname.Text + "' and last_name = '" + txtLname.Text + "'";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    con.Open();
+                    SqlDataReader sdr = cmd.ExecuteReader();
+                    while (sdr.Read())
+                    {
+                        fname = (string)sdr["first_name"];
+                        lname = (string)sdr["last_name"];
+                        age = (int)sdr["age"];
+                        blood_group = (string)sdr["blood_group"];
+                    }
+                    con.Close();
+                }  
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error : " + ex.Message);
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if(fname == null && lname == null)
+            {
+                Response.Write("Patient does not exist");
+                Button AddPatient = new Button();
+                AddPatient.Text = "Add Patient";
+                //AddPatient.Left = 10; AddPatient.Top = 10;  //the button's location
+                AddPatient.Click += new EventHandler(btnAddPtient_Click); 
+                this.Controls.Add(AddPatient);
+            }
+            else
+            {
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = ConfigurationManager.ConnectionStrings["Clinic"].ConnectionString;
+                try
+                {
+                    string query = "insert into appointment (fname, lname, age, blood_group, symptoms)" +
+                        "values(@fname, @lname, @age, @blood_group, @symptoms)";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@fname", fname);
+                    cmd.Parameters.AddWithValue("@lname", lname);
+                    cmd.Parameters.AddWithValue("@age", age);
+                    cmd.Parameters.AddWithValue("@blood_group", blood_group);
+                    cmd.Parameters.AddWithValue("@symptoms", txtSymptoms.Text);
+                    cmd.ExecuteNonQuery();
+
+                    con.Close();
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("Error : " + ex.Message);
+                }
+            }
+            
+        }
+
+        protected void btnAddPtient_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/pages/receptionist/addNewPatient.aspx");
+        }
+    }
+}
